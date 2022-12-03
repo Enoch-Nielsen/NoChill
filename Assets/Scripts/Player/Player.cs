6160af0eacbc89d4eab2305943fb5d4932aeed45
Enoch-Nielsen <93846909+Enoch-Nielsen@users.noreply.github.com>
2022-12-03 12:14:58 -0700

    Changes

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,18 +1,96 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private PlayerMove playerMove;
+    
+    [SerializeField] private float rage, maxRage;
+    [SerializeField] private float rageSpeed, slowedRageSpeed;
+    
+    [SerializeField] private bool isSlowed;
+    [SerializeField] private float slowTimer, slowTimerMax;
+
+    public bool isInvincible { get; private set;}
+    [SerializeField] private float invincibilityTimer, invincibilityMax;
+
+    public bool punchReady { get; private set; }
+
+    private void Update()
     {
+        if (rage < maxRage)
+            rage += (isSlowed ? rageSpeed : slowedRageSpeed) * Time.deltaTime;
+        else
+            punchReady = true;
+
+        if (slowTimer < slowTimerMax)
+            slowTimer += Time.deltaTime;
+        else
+            isSlowed = false;
         
+        if (invincibilityTimer < invincibilityMax)
+            invincibilityTimer += Time.deltaTime;
+        else
+            isInvincible = false;
+
+        if (punchReady)
+            SetInvincible(100.0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    // for rage text value.
+    public int GetRageAsInt()
     {
+        return Mathf.RoundToInt(rage);
+    }
+
+    // for rage bar.
+    public float GetRageFill()
+    {
+        return rage / maxRage;
+    }
+
+    // for visual timer as text.
+    public float GetDodgeDelayAsSeconds()
+    {
+        return playerMove.dodgeDelay - playerMove.dodgeDelayTimer;
+    }
+
+    // for visual timer as a bar or circle.
+    public float GetDodgeDelayAsFill()
+    {
+        return playerMove.dodgeDelayTimer / playerMove.dodgeDelay;
+    }
+
+    // Makes the player invincible for the given time.
+    public void SetInvincible(float time)
+    {
+        invincibilityMax = time;
+        invincibilityTimer = 0.0f;
+
+        isInvincible = true;
+    }
+
+    /**
+     * will damage the player by the damageValue, slow will decrease rage increments temporarily, does nothing if the player
+     * is invincible.
+     */
+    public void Damage(float damageValue, bool doesSlow)
+    {
+        if (isInvincible)
+            return;
         
+        rage -= damageValue;
+
+        if (rage < 0)
+        {
+            rage = 0;
+        }
+
+        isSlowed = doesSlow;
+
+        slowTimer = 0.0f;
     }
 }
