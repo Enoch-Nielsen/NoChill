8aabe6d186c4cf49b6b5886c6b88d9c5d6914e6d
Phoenixxi <rowan809xi@gmail.com>
2022-12-04 14:21:53 -0700

    chillscene buttons

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -20,4 +20,9 @@ public class MainMenu : MonoBehaviour
      {
           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
      }
+
+     public void ReturnMenu()
+     {
+          SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+     }
 }
